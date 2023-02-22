0833c68f0bd40a6b253a565d9945c7d5fefa36bc
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-22 21:49:37 +0100

    Add pseudo-code & winning combinations

---
+++ main.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+WINNING_COMBINATIONS = [[1, 2, 3],
+                        [4, 5, 6],
+                        [7, 8, 9],
+                        [1, 4, 7],
+                        [2, 5, 8],
+                        [3, 6, 9],
+                        [1, 5, 9],
+                        [3, 5, 7]].freeze
+
+# Board for Tic Tac Toe
+class Board
+  def initialize; end
+  # create a variable for winning combinations
+  # create a board class
+  # place squares into the class
+  # identify players, and give them markers
+  # print the board
+  # create a method to choose your spot, via input such as 1, or 6
+  # create a method that waits until all spots are marked,
+  # or someone has a winning combination
+end
