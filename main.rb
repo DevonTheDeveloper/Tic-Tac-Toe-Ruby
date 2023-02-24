aba5afc3791b1088e421e90ac5b218c4b5f9d6b4
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-24 18:14:58 +0100

    Add Board class & print board method

--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+# Board for Tic Tac Toe
+class Board
+  def print_board
+    puts " 1 | 2 | 3\n---+---+---\n 4 | 5 | 6\n---+---+---\n 7 | 8 | 9"
+  end
+
+  def initialize
+    board_squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print_board
+  end
+end
